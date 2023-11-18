c3e628710c4f65aa85e29f7f6dec6183b95e52b1
munaimpro <khanmail2599@gmail.com>
2023-11-18 19:38:05 +0600

    status added to response

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,5 +15,15 @@ class ProfileController extends Controller
             'name' => $name,
             'age' => $age
         ];
+
+        $name = 'access_token';
+        $value = '123-XYZ';
+        $minutes = '1';
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response($data, 200)->cookie($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
     }
 }
